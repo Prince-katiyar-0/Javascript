6c27c4c4e54d5dd3efa4baa451309770101dbe3b
Solo <prince.14.katiyar@gmail.com>
2026-02-06 18:03:05 +0000

    commit from vs code

--- 01_basics/05_comparison.js
+++ 01_basics/05_comparison.js
@@ -9,3 +9,7 @@ console.log(null == 0);
 console.log(null >= 0);
 
 // the reason of the  particular output is that an equality check == and comparison < > >= <= work differently comparisons convert null to a number treating as 0 thas why num >=0 is true and null>0 is fallse
+
+
+
+
